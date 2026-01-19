3e9cff2dee13ef8ce0bfeed10cc3cb8ef2e0d657
Y-bot5 <y.botisbig@gmail.com>
2026-01-19 12:28:36 +0800

    nyv

--- firebase.js
+++ firebase.js
@@ -10,11 +10,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_REAL_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  appId: "YOUR_REAL_APP_ID"
-};
+    apiKey: "AIzaSyD6bBqUlQe32d7dZRmJane3gAsttTCZyPs",
+      authDomain: "shoop-hu4f.firebaseapp.com",
+        projectId: "shoop-hu4f",
+          storageBucket: "shoop-hu4f.firebasestorage.app",
+            messagingSenderId: "291613688377",
+              appId: "1:291613688377:web:6beb609208a1e22da403e8"
+              };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
