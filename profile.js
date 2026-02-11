b91ec4a44a2ac3304113318a707e3070ec74859e
Y-bot5 <y.botisbig@gmail.com>
2026-02-11 15:13:28 +0800

    xdhfgjcvhkj

--- profile.js
+++ profile.js
@@ -1,12 +1,14 @@
 import { auth, onAuthStateChanged } from "/Shoop/firebase.js";
 const profileLink = document.getElementById("profileLink");
 const profileIcon = document.getElementById("avatar");
+const topbar = document.querySelector(".topbar");
 
 onAuthStateChanged(auth, user => {
   if (user) {
     profileLink.href =
       `/Shoop/profile/?u=${encodeURIComponent(user.displayName)}`;
     profileIcon.src = user.photoURL || "/Shoop/noProfile.webp";
+    topbar.style.display = "block";
   } else {
     profileLink.href = "/Shoop/login/";
     profileIcon.src = "/Shoop/noProfile.webp";
